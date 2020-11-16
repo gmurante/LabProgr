1c4a73b40d7632383ff903448bc5b509c9c38d36
GozDavid <david.goz@inaf.it>
2020-11-16 10:52:47 +0100

    Mandelbrot set OpenMP exercise added

--- exercises/sources/OpenMP/solution/mandelbrot_parallel.c
+++ exercises/sources/OpenMP/solution/mandelbrot_parallel.c
@@ -5,10 +5,10 @@
 /* Correct answer should be around 1.510659.                                     */
 /*                                                                               */
 /* Compile the code:                                                             */
-/* $ gcc -Wall -Wextra mandelbrot_serial.c -o mandelbrot_serial                  */
+/* $ gcc -Wall -Wextra -fopenmp mandelbrot_parallel.c -o mandelbrot_parallel     */
 /*                                                                               */
 /* Run the code:                                                                 */
-/* $ time ./mandelbrot_serial                                                    */
+/* $ time ./mandelbrot_parallel <number of OpenMP threads>                       */
 /*                                                                               */
 /* Author: David Goz - david.goz@inaf.it                                         */
 /*                                                                               */
@@ -16,26 +16,31 @@
 /*          Changed by Tim Mattson, September 2011.                              */
 /*          Modified for the purpose of the course by David Goz.                 */
 /*                                                                               */
-/* Assigned tasks:                                                               */
-/*                 - turning on compiler optimizations:                          */
-/*                    - compile the code using the flag -O0 (no optimization),   */
-/*                      run the executable, check the result, and get the time   */
-/*                      execution using the 'time' command;                      */
-/*                    - compile the code using the flag -O3 (with optimization), */
-/*                      run the executable, check the result, and get the time   */
-/*                      execution using the 'time' command;                      */
+/* OpenMP parallelization:                                                       */
+/*                    - the outermost for loop inside the main function is       */
+/*                      parallelized. Loop counters i and j are already private  */
+/*                      since they are decleared inside the parallel region      */
+/*                      (OpenMP rule). The global complex number Z is promoted   */
+/*                      to be private inside the parallel region.                */
+/*                      The only serial code change required is in the           */
+/*                      'testpoint' function argument. In the serial code, the   */
+/*                      'testpoint' function has access to the global complex    */
+/*                      point Z, which is shared among OpenMP threads. For this  */
+/*                      reason, first Z is decleared private to each threads in  */
+/*                      the parallel region, and second Z is passed as argument  */
+/*                      to the 'testpoint' function (i.e. assigned to the stack  */
+/*                      of each thread). This way guarantees that Z is private   */
+/*                      to each thread. Lastly, the update of NumOutside counter */
+/*                      must be atomic inside the 'testpoint' function.          */
 /*                                                                               */
-/*                  - OpenMP parallelization:                                    */
-/*                    - parallelize the code targeting the for loop inside the   */
-/*                      main function. Try to change as little as possible the   */
-/*                      serial code, verifying result correctness.               */
-/*                    - get time measurements using a different number of        */
-/*                      OpenMP threads, different number of complex points       */
-/*                      (NPOINTS macro), and different compiler optimizations.   */
+/*                    - question:                                                */
+/*                                - why does the dynamic schedule guarantee the  */
+/*                                  best performance in terms of time to         */
+/*                                  solution?                                    */
 /*********************************************************************************/
 
-#include <stdio.h>
-#include <stdlib.h>
+/* header required for OpenMP */
+#include "my_omp_header.h"
 
 #define NPOINTS 1000
 #define MAXITER 1000
@@ -52,30 +57,49 @@ typedef struct
   MyFloat i; /* imaginary part */
 } complex;
 
-/* global complex point */
+/* global structure to store a point */
 complex Z;
 /* global number of points outside the Mandelbrot set */
 unsigned int NumOutside = 0;
 
 /* function prototype */
-void testpoint(void);
+void testpoint(const complex);
 
-int main()
+int main(int argc, char *argv[])
 {
+  if (argc < 2)
+    {
+      printf("\n\t Usage: <executable> <number of OpenMP threads> \n\n");
+      return -1;
+    }
+
+  const int num_threads = atoi(argv[1]);
+  
   /* Loop over grid of points in the complex plane that contains */
   /* the Mandelbrot set. For each point check if it is inside or */
   /* outside the set.                                            */
 
-  for (int i=0 ; i<NPOINTS ; i++)
+#pragma omp parallel default(none) private(Z) num_threads(num_threads)
+  {
+#   pragma omp single nowait
     {
-      Z.r = -2.0 + (2.5 *(MyFloat)(i)/(MyFloat)(NPOINTS)) + EPS;
+      /* get the number of active omp threads */
+      printf("\n\t Active OpenMP threads: %d \n",
+	     omp_get_num_threads());
+    } /* omp single nowait */
+
+#   pragma omp for schedule(dynamic) nowait
+    for (int i=0 ; i<NPOINTS ; i++)
+      {
+	Z.r = -2.0 + (2.5 *(MyFloat)(i)/(MyFloat)(NPOINTS)) + EPS;
        
-      for (int j=0 ; j<NPOINTS; j++)
-	{
-	  Z.i = (1.125 * (MyFloat)(j)/(MyFloat)(NPOINTS)) + EPS;
-	  testpoint();
-	}
-    }
+	for (int j=0 ; j<NPOINTS; j++)
+	  {
+	    Z.i = (1.125 * (MyFloat)(j)/(MyFloat)(NPOINTS)) + EPS;
+	    testpoint(Z);
+	  }
+      }
+  } /* omp parallel region */
 
   /* Calculate the area of the Mandelbrot set and the relative error */
   const MyFloat area = 2.0 * 2.5 * 1.125 * (MyFloat)((NPOINTS * NPOINTS) - NumOutside)/(MyFloat)(NPOINTS * NPOINTS);
@@ -87,15 +111,16 @@ int main()
   return 0;
 }
 
-void testpoint(void)
+void testpoint(const complex P)
 {
   /* Perform the iteration Z = Z*Z + C, until |Z|>2 when */ 
   /* the complex point is outside the Mandelbrot set.    */
   /* However, if the loop count reaches MAXCOUNT, then   */
   /* the point is considered to be inside the set.       */
 
-  /* local copy of Z */
-  complex C = Z;
+  /* local copy of complex point */
+  complex C = P;
+  const complex Z = P;
 
   /* initialize the counter */     
   unsigned int counter = 0;
@@ -115,7 +140,10 @@ void testpoint(void)
     }
 
   if (MOD(C.r, C.i) > 4.0)
-    NumOutside++;
+    {
+#   pragma omp atomic
+      NumOutside++;
+    }
 
   return;
 }
